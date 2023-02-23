3e83e96c950e7dafd33916a82ed57782d647efe1
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2023-02-23 15:55:38 -0500

    roompainting solved

---
+++ src/roompainting.java
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class roompainting {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int numsizes = scan.nextInt();
+        int numneed = scan.nextInt();
+
+        for (int i = 0; i < numsizes; i++) {
+
+        }
+    }
+}
